5d4a7b2b6f56da786f11b5abc13be2510cf5a5a8
Matheus Eduardo <matheus.eduardo.silva@ccc.ufcg.edu.br>
2022-01-19 11:26:06 -0300

    Hook de usuários e iniciando paginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,29 +4,10 @@ import { Header } from "../../components/Header/Header"
 import { Pagination } from "../../components/Pagination/Pagination"
 import { SideBar } from "../../components/Sidebar/Sidebar"
 import Link from "next/link";
-import { useEffect } from "react"
-import {useQuery} from "react-query"
-import { api } from "../../services/api"
+import { useUsers } from "../../hooks/useUsers"
 
 export default function UserList(){
-    const {data, isLoading, isFetching ,error} = useQuery('users', async ()=>{
-        const {data} = await api.get("users")
-        const users = data.users.map(user =>{
-            return{
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year:'numeric'
-                })
-            }
-        });
-        
-        return users;   
-    }, {
-        staleTime: 1000*5})
+    const {data, isLoading, isFetching ,error} = useUsers();
 
     const isWideVersion = useBreakpointValue({
         base: false,
